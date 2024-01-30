ad6b0fd9da32fbc6b2439ea68dbc63ee8f60cce3
Majid Hasan <majidhasan@Majids-MacBook-Pro.local>
2024-01-30 11:16:33 +0600

    Finished the first milestone

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const cors = require("cors");
 const express = require("express");
 const colors = require("colors");
 const dotenv = require("dotenv").config();
@@ -10,6 +11,7 @@ connectDB();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
